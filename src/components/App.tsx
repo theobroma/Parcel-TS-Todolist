b4ff4d6db8ad44e0c40dd86e2be35c7998f7db59
Alexandr Siryj <theobroma222@gmail.com>
2020-02-07 10:15:02 +0200

    add index.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+// import { hot } from 'react-hot-loader';
+// import MainApp from '../containers/MainAppContainer';
+
+// const reactLogo = require('./../assets/img/react_logo.svg');
+import "./../assets/scss/index.scss";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <section className="todoapp">
+          {/* <MainApp /> */}
+          1231231231
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { render } from "react-dom";
+// import { Provider } from "react-redux";
+import App from "./components/App";
+// import configureStore from './configureStore';
+
+const rootEl = document.getElementById("root");
+
+// const store = configureStore();
+
+// render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   rootEl,
+// );
+
+render(<App />, rootEl);
