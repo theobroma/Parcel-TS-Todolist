09b2c2b660179062b861ccd46c4c2d95d8a39d8c
Alexandr <theobroma222@gmail.com>
2020-05-08 02:20:11 +0300

    FilterLink to FC

--- src/components/FilterLink.tsx
+++ src/components/FilterLink.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import classnames from 'classnames';
 
-export default class FilterLink extends React.Component<any, any> {
-  render() {
-    const { setFilter, type, filter } = this.props;
-    return (
-      <li>
-        <a
-          href="#"
-          onClick={() => setFilter(type)}
-          className={classnames({ selected: filter === type })}
-        >
-          {this.props.children}
-        </a>
-      </li>
-    );
-  }
+interface Props {
+  // type: string;
+  // filter: string;
+  // setFilter: (type: any) => void; //TODO:
 }
+
+const FilterLink: React.FC<Props> = ({ setFilter, type, filter, children }) => (
+  <li>
+    <a
+      href="#"
+      onClick={() => setFilter(type)}
+      className={classnames({ selected: filter === type })}
+    >
+      {children}
+    </a>
+  </li>
+);
+
+export default FilterLink;
