224db3bab16681250eed1d222e7c7be133f244d2
Alexandr <theobroma222@gmail.com>
2020-05-26 15:01:02 +0300

    add jest&enzyme

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,8 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import MainAppContainer from './containers/MainAppContainer';
-import configureStore from './configureStore';
+import App from './App';
 // All styles
 import './assets/scss/index.scss';
 
 const rootEl = document.getElementById('root');
-
-const store = configureStore();
-
-render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <MainAppContainer />
-    </Provider>
-  </React.StrictMode>,
-  rootEl,
-);
+render(<App />, rootEl);
