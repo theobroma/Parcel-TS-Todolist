50995d19b87a322ae0f70fcab849f7afd1b4b5b0
Alexandr <theobroma222@gmail.com>
2020-06-22 18:01:45 +0300

    Updates

---
+++ precache-manifest.9f142df1c0c76c5f1f573db8e96442db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf62ff7b0bef4adfe86b9f73f3fed4c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b06905c2ed8cf0e8f2e2",
+    "url": "/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "dddd4a65e8a5f763595a",
+    "url": "/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "b06905c2ed8cf0e8f2e2",
+    "url": "/static/js/2.92595149.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.92595149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddd4a65e8a5f763595a",
+    "url": "/static/js/main.9e7ebfbf.chunk.js"
+  },
+  {
+    "revision": "7e05b14c0f9db87ab60e",
+    "url": "/static/js/runtime-main.35683489.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-ts-todolist-with-hooks/precache-manifest.7be75e5c80e359ad3604cb3a4d83166b.js"
+  "/precache-manifest.9f142df1c0c76c5f1f573db8e96442db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ts-todolist-with-hooks/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
