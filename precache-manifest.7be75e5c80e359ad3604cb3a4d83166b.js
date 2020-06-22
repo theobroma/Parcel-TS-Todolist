5f9efe685a6405fc66b59fdd9ec2f3cfa9e8222b
Alexandr <theobroma222@gmail.com>
2020-06-22 17:50:42 +0300

    Updates

---
+++ precache-manifest.7be75e5c80e359ad3604cb3a4d83166b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa5dcb805cc064488b8d0569f8030ef",
+    "url": "/react-ts-todolist-with-hooks/index.html"
+  },
+  {
+    "revision": "0256dec95849dafb5a8c",
+    "url": "/react-ts-todolist-with-hooks/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "d40f8721e9025fa5cde6",
+    "url": "/react-ts-todolist-with-hooks/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "0256dec95849dafb5a8c",
+    "url": "/react-ts-todolist-with-hooks/static/js/2.7d7d9c23.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-ts-todolist-with-hooks/static/js/2.7d7d9c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40f8721e9025fa5cde6",
+    "url": "/react-ts-todolist-with-hooks/static/js/main.4107eacf.chunk.js"
+  },
+  {
+    "revision": "054dcd799c4a3f78211e",
+    "url": "/react-ts-todolist-with-hooks/static/js/runtime-main.62ea04eb.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-ts-todolist-with-hooks/precache-manifest.7be75e5c80e359ad3604cb3a4d83166b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ts-todolist-with-hooks/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
